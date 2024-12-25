b31fe227929a8e9db585f91a2e2d3bd470a3c066
janhavi2005 <132899541+janhavi2005@users.noreply.github.com>
2024-12-26 01:00:28 +0530

    Update connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 mongoose.set("strictQuery", false);
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/local", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://janhavi:janhavi@cluster0.b1zt8.mongodb.net/local", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 }, err => {
